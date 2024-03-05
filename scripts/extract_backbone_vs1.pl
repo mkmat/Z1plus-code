a46fd35eae6b02a542c21cb7c3068c06d045fce5
Martin Kröger <mk@mat.ethz.ch>
2024-03-05 17:55:43 +0100

    Add files via upload

--- scripts/extract_backbone_vs1.pl
+++ scripts/extract_backbone_vs1.pl
@@ -1,11 +1,10 @@
 #! /usr/bin/perl
 
-# (c) mk@mat.ethz.ch  19 oct 2023
-# added -ignore-types 29 feb 2024
+# (c) mk@mat.ethz.ch 19 oct 2023
 
 sub USAGE { print<<EOF;
 use as:\n 
-perl $0 <lammps-data-file> [<lammps-dump-trajectory>] [-ignore-types=<type,type,..>]
+perl $0 <lammps-data-file> [<lammps-dump-trajectory>]
 
 (c) mk@mat.ethz.ch 19 oct 2023
 
@@ -21,11 +20,6 @@ For user's convenience the script furthermore creates a file
 backbone-info.txt. For each chain molecule (chain number, number of atoms) 
 it provides a list of consecutively bonded original atom ids that form
 the linear chain. 
-
-Optionally, the script takes the argument -ignore-types=<type,type,..>
-such as -ignore-types=4 or -ignore-types=2,4. In that case the bead type is 
-completely ignored and won't appear in the resulting files. Any corresponding 
-bonds to other bead types are ignored as well.
 EOF
 exit;
 }; 
@@ -35,29 +29,22 @@ sub strip { chomp $_[0]; $_[0]=~s/^\s+//g; $_[0]=~s/\s+$//; $_[0]=~s/\s+/ /g; $_
 if ($#ARGV eq -1) { USAGE; }; 
 $datafile=$ARGV[0]; 
 if (-s "$datafile") { } else { print "missing file $datafile\n"; exit; }; 
-if ($#ARGV > 0) { 
+if ($#ARGV eq 1) { 
     $dumpfile=$ARGV[1]; 
     if (-s "$dumpfile") { } else { print "missing file $dumpfile\n"; exit; };
 }; 
-@ignoretype=(); 
-foreach $arg (@ARGV) { ($field,$value)=split(/=/,$arg);
-    if ($field eq "-ignore-types") { @ignoretype=split(/,/,$value); }; 
-};
-foreach $j (0 .. $#ignoretype) { print "ignoring type $ignoretype[$j] atoms\n"; }; 
 
 # read data file, give away new ids = row number
 open(D,"<$datafile"); $mols=0; $linearatoms=0;
 while (!eof(D)) {
     $line=<D>; $line=strip($line); 
-    if ($line=~/atoms/) { ($original_atoms,$rest)=split(/ /,$line); };
-    if ($line=~/bonds/) { ($original_bonds,$rest)=split(/ /,$line); };  
+    if ($line=~/atoms/) { ($atoms,$rest)=split(/ /,$line); };
+    if ($line=~/bonds/) { ($bonds,$rest)=split(/ /,$line); };  
     if ($line=~/xlo/)   { ($xlo,$xhi,$rest)=split(/ /,$line); };
     if ($line=~/ylo/)   { ($ylo,$yhi,$rest)=split(/ /,$line); };
     if ($line=~/zlo/)   { ($zlo,$zhi,$rest)=split(/ /,$line); };
     if ($line=~/^Atoms/) { $line=<D>;
-        $j = 0; 
-        foreach $jj (1 .. $original_atoms) { $line=<D>; $line=strip($line);
-            $j += 1; 
+        foreach $j (1 .. $atoms) { $line=<D>; $line=strip($line);
             @tmp=split(/ /,$line); 
             if ($#tmp eq 8) {
                 ($id,$mol[$j],$type[$j],$x[$j],$y[$j],$z[$j],$rest)=split(/ /,$line); 
@@ -70,45 +57,21 @@ while (!eof(D)) {
             } else {
                 print "data format problem, we expect either id mol type x y z or id mol type q x y z\n"; exit; 
             };
-            $ignore = 0; 
-            foreach $k (0 .. $#ignoretype) { if ($type[$j] eq $ignoretype[$k]) { $ignore=1; }; };
-            if ($ignore eq 0) {
-                $row[$id] = $j;         # -> newly assigned ids
-                $anchor[$mol[$j]]=$j; 
-                if ($mol[$j]>$mols) { $mols=$mol[$j]; }; 
-            } else {    
-                $row[$id] = 0; 
-                $j -= 1; 
-            }; 
+            $row[$id] = $j;     # -> newly assigned ids
+            $anchor[$mol[$j]]=$j; 
+            if ($mol[$j]>$mols) { $mols=$mol[$j]; }; 
         };
-        $atoms = $j;    # overwriting atoms
     }; 
     if ($line=~/^Bonds/) { $line=<D>;
-        $j = 0; 
-        foreach $jj (1 .. $original_bonds) { $line=<D>; $line=strip($line);
-            $j += 1; 
+        foreach $j (1 .. $bonds) { $line=<D>; $line=strip($line);
             ($bid,$btype,$b1[$j],$b2[$j])=split(/ /,$line);
-            $ignore = 0;
-            foreach $k (0 .. $#ignoretype) { 
-                if ($type[$b1[$j]] eq $ignoretype[$k]) { $ignore=1; };
-                if ($type[$b2[$j]] eq $ignoretype[$k]) { $ignore=1; };
-            };
-            if ($ignore eq 0) { 
-                $b1[$j]=$row[$b1[$j]];
-                $b2[$j]=$row[$b2[$j]];
-            } else {
-                $j -= 1; 
-            };
+            $b1[$j]=$row[$b1[$j]]; 
+            $b2[$j]=$row[$b2[$j]];
         };
-        $bonds = $j;    # overwriting bonds
     };
 };
-if (!$original_atoms) { print "This file $datafile does not seem to be a lammps data file\n"; exit; }; 
-if ($#ignoretype>-1) {
-    print "$atoms/$original_atoms atoms, $bonds/$original_bonds bonds after erasing atom types, largest mol id is $mols\n";
-} else { 
-    print "$atoms atoms, $bonds bonds, largest mol id is $mols\n";
-}; 
+if (!$atoms) { print "This file $datafile does not seem to be a lammps data file\n"; exit; }; 
+print "$atoms atoms, $bonds bonds, largest mol id is $mols\n";
 print "box $xlo $xhi $ylo $yhi $zlo $zhi\n";
 
 # create connectivity information
@@ -220,8 +183,6 @@ foreach $m (1 .. $mols) {
 
 };
 
-print "$linearatoms linear atoms\n"; 
-
 # save Z1-formatted data file
 $molecules = $#N+1; 
 $boxx = $xhi-$xlo;
@@ -243,7 +204,7 @@ if (-s "$dumpfile") {
         $line=<D>; $line=strip($line); 
         if      ($line =~ /NUMBER OF ATOMS/) { $line=<D>; $line=strip($line);
             $dumpatoms=$line+0; 
-            if ($dumpatoms eq $original_atoms) { } else { print "conflicting data and dump files [$original_atoms versus $dumpatoms atoms]\n"; exit; }; 
+            if ($dumpatoms eq $atoms) { } else { print "conflicting data and dump files [$atoms versus $dumpatoms atoms]\n"; exit; }; 
         } elsif ($line =~ /ITEM: ATOMS id/) {
             @XYZ=();
             @tmp=split(/ /,$line);   
@@ -252,21 +213,17 @@ if (-s "$dumpfile") {
                 if (($tmp[$k] eq "x")||($tmp[$k] eq "xu")) { $col=$k-2; };
             };
             if ($col eq -1) { print "format error in dumpfile $dumpfile (must contain x or xu)\n"; };
-            foreach $j (1 .. $original_atoms) {
+            foreach $j (1 .. $atoms) {
                 $line=<D>; $line=strip($line);
                 @tmp   = split(/ /,$line);
                 $id    = $row[$tmp[0]]; 
-                if ($id) { 
-                    $xyz   = "$tmp[$col] $tmp[$col+1] $tmp[$col+2]";
-                    if ($LINEAR_mol[$id]) { $XYZ[$LINEAR_id[$id]] = "$xyz\n"; }; 
-                }; 
+                $xyz   = "$tmp[$col] $tmp[$col+1] $tmp[$col+2]";
+                if ($LINEAR_mol[$id]) { $XYZ[$LINEAR_id[$id]] = "$xyz\n"; }; 
             };
             print Z1 "$molecules\n$boxx $boxy $boxz\n";
             foreach $j (0 .. $#N)   { print Z1 "$N[$j] "; }; print Z1 "\n";
             print Z1 @XYZ; 
             # print Z1 "0\n";     # separator
-        } elsif ($line =~ /TIMESTEP/) {
-            $line=<D>+0; print "[$dumpfile] processing time step $line\n"; 
         };
     };  
     close(D);
